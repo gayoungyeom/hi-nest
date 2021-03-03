a4a413896df110529fefe35623823555536e9010
gayoungyeom <gayoungyeom@gmail.com>
2021-03-03 23:16:53 +0900

    Create movie api

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly movieService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.movieService.getAll();
+  }
+
+  //search 부분이 get보다 밑에 있으면 nestjs는 search를 id로 판단한다.
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching  for a movie made after: ${searchingYear}`;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: number): Movie {
+    return this.movieService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData: CreateMovieDto) {
+    return this.movieService.create(movieData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') movieId: number) {
+    return this.movieService.deleteOne(movieId);
+  }
+
+  @Patch(':id')
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+    return this.movieService.update(movieId, updateData);
+  }
+}
